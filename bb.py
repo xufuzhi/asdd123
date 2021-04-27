6e76c824e312284981534954c607a5b66157f7a8
xufuzhi <xfz316@gmail.com>
2021-04-27 14:47:20 +0800

    ok

--- bb.py
+++ bb.py
@@ -2,22 +2,83 @@ import lmdb
 import numpy as np
 import cv2 as cv
 from itertools import islice
+import time
 
 import os
 from os.path import sep as path_sep
 from os.path import join as path_join
 
-
-
 import torch
 import torch.nn as nn
 import torchvision
 
-net = torchvision.models.vgg16_bn(pretrained=False)
-cnn = net.features
-cnn[6] = nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1), padding=0, dilation=1, ceil_mode=False)
-cnn[23] = nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1), padding=0, dilation=1, ceil_mode=False)
-cnn[33] = nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1), padding=0, dilation=1, ceil_mode=False)
+###################################################
+import models.crnn as crnn
+
+class print_time():
+    t = 0
+
+    @classmethod
+    def strat(cls):
+        torch.cuda.synchronize()
+        cls.t = time.time()
+
+    @classmethod
+    def end(cls):
+        torch.cuda.synchronize()
+        print('use time: ', time.time() - cls.t)
+
+    # 读取字母表
+with open('data/en.alphabet', encoding='utf-8') as f:
+    alphabet = f.read().strip()
+
+net_crnn = crnn.CRNN_res50_pp(32, 3, len(alphabet) + 1, 256, d_bug='maxpool', rudc=False).to('cuda').eval()
+# net_crnn = crnn.CRNN(32, 3, len(alphabet) + 1, 256).to('cuda').eval()
+
+x = torch.rand([100, 3, 32, 128]).cuda()
+
+with torch.no_grad():
+    y = net_crnn(x)
+
+    torch.cuda.synchronize()
+    t1 = time.time()
+    for _ in range(10):
+        y = net_crnn(x)
+    torch.cuda.synchronize()
+    print('time: ', time.time() - t1)
+
+raise ValueError
+
+##################################################################
+
+import copy
+net = torchvision.models.resnet50(pretrained=False)
+new_1 = copy.deepcopy(net.layer1[0])
+new_1.conv1 = nn.Conv2d(32, 32, kernel_size=(1, 1), stride=(1, 1), bias=False)
+new_1.bn1 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+new_1.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+new_1.bn2 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+new_1.conv3 = nn.Conv2d(32, 64, kernel_size=(1, 1), stride=(1, 1), bias=False)
+new_1.bn3 = nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+new_1.downsample[0] = nn.Conv2d(32, 64, kernel_size=(1, 1), stride=(1, 1), bias=False)
+new_1.downsample[1] = nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+
+l1 = nn.Sequential(
+    nn.Conv2d(3, 32, kernel_size=7, stride=1, padding=3, bias=False),
+    nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+    nn.ReLU(inplace=True),
+    new_1
+)
+
+cnn = nn.Sequential(l1, *(list(net.children())[3: -2]))
+# 修改网络层
+cnn[3][0].conv2.stride = (2, 1)
+cnn[3][0].downsample[0].stride = (2, 1)
+cnn[4][0].conv2.stride = (2, 1)
+cnn[4][0].downsample[0].stride = (2, 1)
+cnn[5][0].conv2.stride = (2, 1)
+cnn[5][0].downsample[0].stride = (2, 1)
+cnn.add_module('maxPooling', nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
 
 x = torch.rand(1, 3, 32, 128)
 y = cnn(x)
--- models/crnn.py
+++ models/crnn.py
@@ -1,3 +1,6 @@
+import time
+
+import torch.cuda
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models
@@ -366,3 +369,111 @@ class CRNN_m(nn.Module):
 
         return output
 
+
+###########################################################################################################
+class CRNN_res50_pp(nn.Module):
+    def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False, d_bug='maxpool', rudc=True):
+        print('CRNN_res50_pp')
+        super(CRNN_res50_pp, self).__init__()
+        assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
+
+        net = torchvision.models.resnet50(pretrained=False)
+        l1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False),
+        )
+        cnn = nn.Sequential(*(list(net.children())[: -2]))
+        # 修改网络层
+        cnn[0] = l1
+        cnn[5][0].conv2.stride = (2, 1)
+        cnn[5][0].downsample[0].stride = (2, 1)
+        cnn[6][0].conv2.stride = (2, 1)
+        cnn[6][0].downsample[0].stride = (2, 1)
+        cnn[7][0].conv2.stride = (2, 1)
+        cnn[7][0].downsample[0].stride = (2, 1)
+        cnn.add_module('maxPooling', nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+
+        self.cnn = cnn
+        self.rnn = nn.Sequential(
+            BidirectionalLSTM(2048, nh, nh),
+            BidirectionalLSTM(nh, nh, nclass)
+        )
+
+    def forward(self, input):
+        # conv features
+        conv = self.cnn(input)
+        b, c, h, w = conv.size()
+        assert h == 1, "the height of conv must be 1"
+        conv = conv.squeeze(2)
+        conv = conv.permute(2, 0, 1)  # [w, b, c]
+
+        # rnn features
+        output = self.rnn(conv)
+
+        # add log_softmax to converge output
+        # output = F.log_softmax(output, dim=2)
+
+        return output
+
+
+###########################################################################################################
+class CRNN_res50_1(nn.Module):
+    def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False, d_bug='maxpool', rudc=True):
+        print('CRNN_res50_1')
+        super(CRNN_res50_1, self).__init__()
+        assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
+
+        net = torchvision.models.resnet50(pretrained=False)
+        new_1 = copy.deepcopy(net.layer1[0])
+        new_1.conv1 = nn.Conv2d(32, 32, kernel_size=(1, 1), stride=(1, 1), bias=False)
+        new_1.bn1 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        new_1.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+        new_1.bn2 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        new_1.conv3 = nn.Conv2d(32, 64, kernel_size=(1, 1), stride=(1, 1), bias=False)
+        new_1.bn3 = nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        new_1.downsample[0] = nn.Conv2d(32, 64, kernel_size=(1, 1), stride=(1, 1), bias=False)
+        new_1.downsample[1] = nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+
+        l1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=7, stride=1, padding=3, bias=False),
+            nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ReLU(inplace=True),
+            new_1
+        )
+
+        cnn = nn.Sequential(l1, *(list(net.children())[3: -2]))
+        # 修改网络层
+        cnn[3][0].conv2.stride = (2, 1)
+        cnn[3][0].downsample[0].stride = (2, 1)
+        cnn[4][0].conv2.stride = (2, 1)
+        cnn[4][0].downsample[0].stride = (2, 1)
+        cnn[5][0].conv2.stride = (2, 1)
+        cnn[5][0].downsample[0].stride = (2, 1)
+        cnn.add_module('maxPooling', nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+
+        self.cnn = cnn
+        self.rnn = nn.Sequential(
+            BidirectionalLSTM(2048, nh, nh),
+            BidirectionalLSTM(nh, nh, nclass)
+        )
+
+    def forward(self, input):
+        # conv features
+        conv = self.cnn(input)
+        b, c, h, w = conv.size()
+        assert h == 1, "the height of conv must be 1"
+        conv = conv.squeeze(2)
+        conv = conv.permute(2, 0, 1)  # [w, b, c]
+
+        # rnn features
+        output = self.rnn(conv)
+
+        # add log_softmax to converge output
+        # output = F.log_softmax(output, dim=2)
+
+        return output
